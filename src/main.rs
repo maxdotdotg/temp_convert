3410d96c77d93fadc51e9c940e685bba4e91eafd
Max G <maxdotdotg@gmail.com>
2020-01-04 20:24:59 -0500

    add match expression to create clean "unit" variable, and then to match against it when converting temperatures

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::string::String;
 
 fn main() {
-        println!("convert temperatures!");
+    println!("convert temperatures!");
     
     loop {
         println!("how many degrees?");
@@ -22,29 +22,35 @@ fn main() {
         let mut unit = String::new();
         io::stdin().read_line(&mut unit)
             .expect("failed to read line");
-    
-    
-        println!("converting {} degrees {}", temp, unit);
-        // match against unit
-        match unit.trim().to_lowercase() {
-            "f" => 
-            { println!("that's {} degrees {}", to_c(temp), unit);
-                break},
-            "c" => 
-            { println!("that's {} degrees {}", to_f(temp), unit);
-                break},
-            _ => continue,
-        }
 
+        // make sure unit is uppercase and "clean"
+        let unit  = match unit.as_str().to_uppercase().trim() {
+            "F" => {"F"},
+            "C" => {"C"},
+            &_ => continue,
+        };
+        
+        println!("converting {} degrees {}", temp, unit);
+
+        // match against unit
+        match unit {
+            "F" => { println!("that's {} degrees C", to_c(temp));
+                break },
+            "C" => { println!("that's {} degrees F", to_f(temp));
+                break },
+            &_ => { println!("exiting"); break }
+        }
     }
 }
 
 fn to_c(f: i32) -> i32 {
+    // convert F to C
     // (32°F − 32) × 5/9 = 0°C
     (f - 32) * 5/9
 }
 
 fn to_f(c: i32) -> i32 {
+    // convert C to F
     // (0°C × 9/5) + 32 = 32°F
     (c * 9/5) + 32
 }
